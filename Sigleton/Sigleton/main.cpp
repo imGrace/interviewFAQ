2701ada83e4bcdc6613f14d50f714eab6044f3ea
liudi <imliudi@gmail.com>
2013-09-30 15:37:18 +0800

    hi, world

---
+++ Sigleton/Sigleton/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Sigleton
+//
+//  Created by Diane on 9/30/13.
+//  Copyright (c) 2013 Diane liudi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
